1a712200610d3391c8211d4e6ea65de46dbd9ca0
NikahPanjaitan <suchiapanjaitan@gmail.com>
2024-09-04 21:27:12 +0700

    Telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,16 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24041 - Nikah Suchia Panjaitan
+// 12S24041- Nikah Suchia Panjaitan
 
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+
+        nama = input.nextLine();
+        System.out.println(nama);
+    }
+}
